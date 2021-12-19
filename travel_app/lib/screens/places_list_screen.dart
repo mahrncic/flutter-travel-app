75618b739666b29466cdb9b420e158d1b88efda1
Marko Hrncic <marko.hrncic@kogitas.com>
2021-12-19 21:53:40 +0100

    Implementation of initial project structure.

---
+++ travel_app/lib/screens/places_list_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class PlacesListScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Places'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.add),
+          )
+        ],
+      ),
+      body: Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}
